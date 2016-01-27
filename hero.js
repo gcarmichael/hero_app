a031634c986f3cbf5d5e11ed1889763454a69a0a
Sky Su <februaryskys@gmail.com>
2016-01-27 14:35:05 +0000

    Adds method to Hero Prototype to talk

--- hero.js
+++ hero.js
@@ -4,6 +4,12 @@ var Hero = function(name, health, favouriteFood){
   this.favouriteFood = favouriteFood;
 };
 
+Hero.prototype = {
+  talk: function(){
+    return 'My name is ' + this.name + '.';
+  }
+};
+
 var hero1 = new Hero('Gary', 10, 'pasta');
 
 module.exports.hero = hero1;
--- hero_spec.js
+++ hero_spec.js
@@ -5,4 +5,7 @@ describe('Hero', function () {
   it('should have a name, health, and favourite food', function () {
     expect(hero.name).to.equal('Gary', 10, 'pasta');
   });
+  it('should be able to talk and say their own name', function(){
+    expect(hero.talk()).to.equal('My name is Gary.');
+  })
 });
